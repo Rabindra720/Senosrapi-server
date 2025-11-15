605799c175bcf848c4180e64753e88ceea38042c
Rabindra Dhakal <rujandhakal12@gmail.com>
2025-11-15 18:36:23 +0200

    Updated index.html and server.js

--- server.js
+++ server.js
@@ -1,3 +1,50 @@
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const db = require('./database');
+// const WebSocket = require('ws');
+// const apiRoutes = require('./routes/api');
+
+// const app = express();
+// const PORT = 3000;
+
+// app.use(bodyParser.json());
+// app.use('/api', apiRoutes);
+// app.use(express.static('public'));
+
+// // Webhook to receive sensor data
+// app.post('/webhook', (req, res) => {
+//   const { temperature, humidity } = req.body;
+
+//   db.run(
+//     'INSERT INTO readings (temperature, humidity) VALUES (?, ?)',
+//     [temperature, humidity],
+//     (err) => {
+//       if (err) return res.status(500).send('DB error');
+//       broadcast({ temperature, humidity });
+//       res.status(200).send('Data received');
+//     }
+//   );
+// });
+
+// // Start HTTP server
+// const server = app.listen(PORT, () => {
+//   console.log(`Server running at http://192.168.56.1:${PORT}`);
+// });
+
+// // WebSocket setup
+// const wss = new WebSocket.Server({ server });
+
+// function broadcast(data) {
+//   const message = JSON.stringify(data);
+//   wss.clients.forEach((client) => {
+//     if (client.readyState === WebSocket.OPEN) {
+//       client.send(message);
+//     }
+//   });
+// }
+
+
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./database');
@@ -5,13 +52,12 @@ const WebSocket = require('ws');
 const apiRoutes = require('./routes/api');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use('/api', apiRoutes);
 app.use(express.static('public'));
 
-// Webhook to receive sensor data
 app.post('/webhook', (req, res) => {
   const { temperature, humidity } = req.body;
 
@@ -26,16 +72,15 @@ app.post('/webhook', (req, res) => {
   );
 });
 
-// Start HTTP server
 const server = app.listen(PORT, () => {
-  console.log(`Server running at http://192.168.56.1:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
-// WebSocket setup
 const wss = new WebSocket.Server({ server });
 
 function broadcast(data) {
   const message = JSON.stringify(data);
+  console.log("Broadcasting:", message);
   wss.clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN) {
       client.send(message);
